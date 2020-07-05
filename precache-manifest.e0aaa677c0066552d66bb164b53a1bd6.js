0d6216ed6aec2f25a37a96501a3a0b82b2618294
rtsantos-github <roldantsantos@gmail.com>
2020-07-05 19:09:49 +0800

    Updates

---
+++ precache-manifest.e0aaa677c0066552d66bb164b53a1bd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8953b5c2df069a85a8e4de6b1c3cf7e0",
+    "url": "/hoas/index.html"
+  },
+  {
+    "revision": "3f230c368f1900678bfb",
+    "url": "/hoas/static/css/main.f547b944.chunk.css"
+  },
+  {
+    "revision": "930207448894ff6428c2",
+    "url": "/hoas/static/js/2.3a9157b6.chunk.js"
+  },
+  {
+    "revision": "310403ebcb29a22f1df8ccb12865b8e8",
+    "url": "/hoas/static/js/2.3a9157b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f230c368f1900678bfb",
+    "url": "/hoas/static/js/main.a555528e.chunk.js"
+  },
+  {
+    "revision": "0d4526e5428d439a8e80",
+    "url": "/hoas/static/js/runtime-main.8255bf0d.js"
+  },
+  {
+    "revision": "fd3fe931f80ac906324dd60965e77241",
+    "url": "/hoas/static/media/Logo.fd3fe931.png"
+  },
+  {
+    "revision": "bc44f07d1561e4f03247af1ae7f6c2e6",
+    "url": "/hoas/static/media/MommyDaddy.bc44f07d.png"
+  },
+  {
+    "revision": "388f7ce5c07eb346ff05c3d2c335e1d5",
+    "url": "/hoas/static/media/nature.388f7ce5.png"
+  }
+]);
