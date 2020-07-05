0b672eb15d047c7f04046ef18a90f081c7313c01
rtsantos-github <roldantsantos@gmail.com>
2020-07-05 21:00:22 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -24,9 +24,9 @@ serviceWorker.unregister();
 
 //How to Deploy project in Git-Hub
 //1. Install gh-pages =    npm install gh-pages --save-dev
-//2. Create new repository in git-hug,  browse this url given you already login. =    https://github.com/new
-//3. Initialized git, in terminal run = git nit
-//4. Add remote origin to project, in terminal run this command =  git remote add origin https://github.com/rtsantos-github/HOAS.git
+//2. Create new repository in git-hub,  browse this url given you already login. =    https://github.com/new
+//3. Initialized git, in terminal run = git init
+//4. Add remote origin to project, in terminal run this command =  git remote add origin https://github.com/rtsantos-github/hoas.git
 //5. In package.json, add home page before dependencies entry = "homepage": "http://rsantos-github.github.io/hoas"
 //6. In package.json, add predeploy and deploy under Scripts =  "predeploy": "npm run build",
 //  "deploy": "gh-pages -d build"
@@ -36,3 +36,7 @@ serviceWorker.unregister();
 //setup your email and user name in terminal type the following
 //git config --global user.email "roldantsantos@gmail.com"
 //git config --global user.name "rtsantos-github"
+
+//How to commit in github
+//1. git add .
+//2. git commit -m "your comment"
